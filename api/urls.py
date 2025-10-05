1c9b9cfde3ae4113b553d1031b21f8ce4ea46e31
Youssef Elsafty <ahmedelsafty317@gmail.com>
2025-10-05 16:01:40 +0300

    Simplify API to serve data directly from JSON file: - Replace database-based views with direct JSON file access - Update URL configuration - Remove dependency on DZIImage model for image listing

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('images/', views.DZIImageListView.as_view(), name='dzi-image-list'),
+    path('images/', views.ImageListView.as_view(), name='dzi-image-list'),
     path('populate/', views.populate_from_data_json, name='populate-from-json'),
 ]
